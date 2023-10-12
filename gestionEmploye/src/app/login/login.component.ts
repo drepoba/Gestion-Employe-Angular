2782579cb5eb06015219982ac6a6b4bcb94583ce
bahidrepoba <alain-didier.drepoba@nticagileconsulting.com>
2023-10-12 11:17:30 +0200

    add html

--- gestionEmploye/src/app/login/login.component.ts
+++ gestionEmploye/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username:string="";
+  password:string="";
 
+  constructor(private router:Router){}
+  ngOnInit():void{
+
+  }
+
+  onLogin():void{
+    //data group (pool)
+
+}
 }
